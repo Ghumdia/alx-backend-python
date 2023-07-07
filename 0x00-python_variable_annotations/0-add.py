b2fa285d54545e122c8055d6300982c32df484dd
Your Name <you@example.com>
2023-07-07 13:09:31 +0000

    Committed

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -1,4 +1,6 @@
 #!/usr/bin/env python3
+"""Function returns a float"""
+
 def add(a: float, b: float) -> float:
     """
     Returns the sum of two floats
--- 0x00-python_variable_annotations/1-concat.py
+++ 0x00-python_variable_annotations/1-concat.py
@@ -1,4 +1,8 @@
 #!/usr/bin/env python3
+"""
+Function retuens a concatenated string
+"""
+
 def concat(str1: str, str2: str) -> str:
     """
     Concatenate two strings and returns it
