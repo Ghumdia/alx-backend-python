0795310ce408a0487e60c3212e98035e72abd7cb
Your Name <you@example.com>
2023-07-07 15:07:16 +0000

    Committed

--- 0x00-python_variable_annotations/9-element_length.py
+++ 0x00-python_variable_annotations/9-element_length.py
@@ -1,8 +1,15 @@
 #!/usr/bin/env python3
 """
-Function returns something
+Imports multiple functions from the typing module
 """
 from typing import Iterable, Sequence, List, Tuple
+"""
+Function that returns a List
+"""
+
 
 def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
+    """
+    Takes on argument and returns a list
+    """
     return [(i, len(i)) for i in lst]
