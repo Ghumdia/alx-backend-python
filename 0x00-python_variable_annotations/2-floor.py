78f234a52560c364a615d920af8544bcc919b7dc
Your Name <you@example.com>
2023-07-07 13:19:33 +0000

    Committed

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+Importting the math modules
+"""
+import math
+"""
+Function returns the floor of a float
+"""
+
+
+def floor(n: float) -> int:
+    return math.floor(n)
